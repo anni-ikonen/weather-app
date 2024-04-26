e99599e5f1bbc61ca67ee86c94e43c42421cd5b1
anniareyouok <anni.s.ikonen@outlook.com>
2024-04-26 04:39:51 +0300

    Changed screen name

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './screens/Home';
+import Search from './screens/Search';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home/>
+      <Search/>
       <StatusBar style="auto" />
     </View>
   );
