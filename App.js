a2f6697cf75a6a3133fc4f731e3a66db4bb41682
anniareyouok <anni.s.ikonen@outlook.com>
2024-04-26 04:05:31 +0300

    Cleaning

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Home from './screens/Home';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home></Home>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
