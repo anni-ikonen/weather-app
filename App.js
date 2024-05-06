a64df1f70f1c17cc14d2875af87d50ce90087a36
anniareyouok <anni.s.ikonen@outlook.com>
2024-05-07 02:29:11 +0300

    Cleaning code, making it uniform

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import Search from './screens/Search';
-import { PaperProvider } from 'react-native-paper';
+import { StatusBar } from 'expo-status-bar'
+import Search from './screens/Search'
+import { PaperProvider } from 'react-native-paper'
 
 export default function App() {
   return (
